91593a14908352f6b8dc33abdc214ef764b694bc
AM-MUGAMBI <amugambi001@gmail.com>
2022-12-08 12:11:25 -0800

    modi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 /*
  * main - print a  line of code using puts
  *
- * Return (0)
+ * Return(0)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return (0)
-}
-			
+	Return(0)
 
