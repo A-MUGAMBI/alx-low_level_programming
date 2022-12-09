36efab6eff95a97c3adfdf55be90344c7dfcec20
AM-MUGAMBI <amugambi001@gmail.com>
2022-12-09 03:12:44 -0800

    modi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- * Retun: always 0 (success)
+ * main - Entry
+ * Return: Always return 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
